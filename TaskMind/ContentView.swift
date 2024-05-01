eb1e57abd409fc9ddb602ce337b353de9830bee8
AleksandraRusak <rusakaleksandra94@gmail.com>
2024-05-01 21:58:15 +0200

    Initial Commit

---
+++ TaskMind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskMind
+//
+//  Created by Aleksandra Rusak on 2024-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskMind/TaskMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMindApp.swift
+//  TaskMind
+//
+//  Created by Aleksandra Rusak on 2024-05-01.
+//
+
+import SwiftUI
+
+@main
+struct TaskMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
