09c33ce1046aa24d55b7a21eff21d588f0e6ac2a
AleksandraRusak <rusakaleksandra94@gmail.com>
2024-05-07 13:25:50 +0200

    added swipe to delete and edit todos

--- TaskMind/ViewModels/ToDoListViewViewModel.swift
+++ TaskMind/ViewModels/ToDoListViewViewModel.swift
@@ -5,100 +5,100 @@
 //  Created by Aleksandra Rusak on 2024-05-02.
 //
 
-import Foundation
-import FirebaseFirestore
-import FirebaseAuth
-
-// ViewModel for list of all todos
-class ToDoListViewViewModel: ObservableObject {
-    
-    @Published var showingNewItemView = false
-    
-    
-    private let userId: String
-    
-    init(userId: String) {
-        self.userId = userId
-    }
-    
-    // Delete to do list item
-    func delete(id: String) {
-        let db = Firestore.firestore()
-        
-        db.collection("users")
-            .document(userId)
-            .collection("todos")
-            .document(id)
-            .delete()
-    }
-    
-    func toggleIsDone(item: ToDoListItem) {
-            var itemCopy = item
-            itemCopy.isDone.toggle()
-            
-            let db = Firestore.firestore()
-            guard let uid = Auth.auth().currentUser?.uid else {
-                return
-            }
-            
-            db.collection("users")
-                .document(uid)
-                .collection("todos")
-                .document(itemCopy.id)
-                .setData(itemCopy.asDictionary())
-        }
-}
-
-
 //import Foundation
 //import FirebaseFirestore
 //import FirebaseAuth
 //
+//// ViewModel for list of all todos
 //class ToDoListViewViewModel: ObservableObject {
-//    @Published var items: [ToDoListItem] = []  // This needs to be fetched appropriately
+//    
 //    @Published var showingNewItemView = false
-//
+//    
+//    
 //    private let userId: String
-//    private var db = Firestore.firestore()
-//
+//    
 //    init(userId: String) {
 //        self.userId = userId
-//        fetchItems()
 //    }
-//
-//    func fetchItems() {
-//        db.collection("users").document(userId).collection("todos")
-//            .whereField("isDone", isEqualTo: false)  // Only fetch incomplete items
-//            .addSnapshotListener { querySnapshot, error in
-//                if let querySnapshot = querySnapshot {
-//                    self.items = querySnapshot.documents.compactMap { document in
-//                        try? document.data(as: ToDoListItem.self)
-//                    }
-//                }
-//            }
-//    }
-//
-//    func toggleIsDone(item: ToDoListItem) {
-//        guard let index = items.firstIndex(where: { $0.id == item.id }) else { return }
-//
-//        // Optimistically update the UI
-//        items[index].isDone = true
-//
-//        // Delay the Firestore update
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//            self.updateItemInFirestore(item: self.items[index])
-//            self.items.remove(at: index)  // Remove from the local array after 2 seconds
-//        }
-//    }
-//
-//    private func updateItemInFirestore(item: ToDoListItem) {
-//        db.collection("users").document(userId).collection("todos")
-//            .document(item.id)
-//            .updateData(["isDone": true])  // Set isDone to true in Firestore
-//    }
-//
+//    
+//    // Delete to do list item
 //    func delete(id: String) {
-//        db.collection("users").document(userId).collection("todos")
-//            .document(id).delete()
+//        let db = Firestore.firestore()
+//        
+//        db.collection("users")
+//            .document(userId)
+//            .collection("todos")
+//            .document(id)
+//            .delete()
 //    }
+//    
+//    func toggleIsDone(item: ToDoListItem) {
+//            var itemCopy = item
+//            itemCopy.isDone.toggle()
+//            
+//            let db = Firestore.firestore()
+//            guard let uid = Auth.auth().currentUser?.uid else {
+//                return
+//            }
+//            
+//            db.collection("users")
+//                .document(uid)
+//                .collection("todos")
+//                .document(itemCopy.id)
+//                .setData(itemCopy.asDictionary())
+//        }
 //}
+
+
+import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class ToDoListViewViewModel: ObservableObject {
+    @Published var items: [ToDoListItem] = []  // This needs to be fetched appropriately
+    @Published var showingNewItemView = false
+
+    private let userId: String
+    private var db = Firestore.firestore()
+
+    init(userId: String) {
+        self.userId = userId
+        fetchItems()
+    }
+
+    func fetchItems() {
+        db.collection("users").document(userId).collection("todos")
+            .whereField("isDone", isEqualTo: false)  // Only fetch incomplete items
+            .addSnapshotListener { querySnapshot, error in
+                if let querySnapshot = querySnapshot {
+                    self.items = querySnapshot.documents.compactMap { document in
+                        try? document.data(as: ToDoListItem.self)
+                    }
+                }
+            }
+    }
+
+    func toggleIsDone(item: ToDoListItem) {
+        guard let index = items.firstIndex(where: { $0.id == item.id }) else { return }
+
+        // Optimistically update the UI
+        items[index].isDone = true
+
+        // Delay the Firestore update
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.updateItemInFirestore(item: self.items[index])
+            self.items.remove(at: index)  // Remove from the local array after 2 seconds
+        }
+    }
+
+    private func updateItemInFirestore(item: ToDoListItem) {
+        db.collection("users").document(userId).collection("todos")
+            .document(item.id)
+            .updateData(["isDone": true])  // Set isDone to true in Firestore
+    }
+
+    func delete(id: String) {
+        db.collection("users").document(userId).collection("todos")
+            .document(id).delete()
+    }
+}
--- TaskMind/Views/ToDoListView.swift
+++ TaskMind/Views/ToDoListView.swift
@@ -5,80 +5,6 @@
 //  Created by Aleksandra Rusak on 2024-05-02.
 //
 
-import SwiftUI
-import FirebaseFirestoreSwift
-
-struct ToDoListView: View {
-    @StateObject var viewModel: ToDoListViewViewModel
-    @FirestoreQuery var items: [ToDoListItem]
-    @State private var searchText = ""
-    
-    @State private var editingItem: ToDoListItem? // For editing existing items
-
-
-    init(userId: String) {
-        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
-        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
-    }
-
-    var body: some View {
-        NavigationStack {
-            List {
-                ForEach(filteredItems) { item in
-                    ToDoListItemView(item: item, toggleIsDone: {
-                                           viewModel.toggleIsDone(item: item)
-                                       })
-                    .swipeActions(edge: .trailing) {
-                                            Button("Delete") {
-                                                viewModel.delete(id: item.id)
-                                            }
-                                            .tint(.red)
-                                        }
-                                        .swipeActions(edge: .leading) {
-                                            Button("Edit") {
-                                                editingItem = item
-                                                viewModel.showingNewItemView = true
-                                            }
-                                            .tint(.blue)
-                        }
-                }
-            }
-            .listStyle(PlainListStyle())
-            .navigationTitle("To Do List")
-            .searchable(text: $searchText) // Using the searchable modifier
-            .toolbar {
-                Button {
-                    editingItem = nil // Reset for adding new item
-                    viewModel.showingNewItemView = true
-                } label: {
-                    Image(systemName: "plus")
-                        .foregroundColor(Color.myBlue)
-                }
-            }
-            .sheet(isPresented: $viewModel.showingNewItemView, content: {
-                NewItemView(newItemPresented: $viewModel.showingNewItemView, itemToEdit: editingItem)
-            })
-        }
-    }
-
-    private var filteredItems: [ToDoListItem] {
-        if searchText.isEmpty {
-            return items.sorted(by: { $0.dueDate > $1.dueDate })
-        } else {
-            return items.filter { $0.title.localizedCaseInsensitiveContains(searchText) }
-                        .sorted(by: { $0.dueDate > $1.dueDate })
-        }
-    }
-}
-
-struct ToDoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ToDoListView(userId: "ICm3vTb0VEX3P5rgioEWy3nhJNm1")
-    }
-}
-
-
-
 //import SwiftUI
 //import FirebaseFirestoreSwift
 //
@@ -88,33 +14,33 @@ struct ToDoListView_Previews: PreviewProvider {
 //    @State private var searchText = ""
 //    
 //    @State private var editingItem: ToDoListItem? // For editing existing items
-//    
-//    
+//
+//
 //    init(userId: String) {
 //        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
 //        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
 //    }
-//    
+//
 //    var body: some View {
 //        NavigationStack {
 //            List {
 //                ForEach(filteredItems) { item in
 //                    ToDoListItemView(item: item, toggleIsDone: {
-//                        viewModel.toggleIsDone(item: item)
-//                    })
+//                                           viewModel.toggleIsDone(item: item)
+//                                       })
 //                    .swipeActions(edge: .trailing) {
-//                        Button("Delete") {
-//                            viewModel.delete(id: item.id)
+//                                            Button("Delete") {
+//                                                viewModel.delete(id: item.id)
+//                                            }
+//                                            .tint(.red)
+//                                        }
+//                                        .swipeActions(edge: .leading) {
+//                                            Button("Edit") {
+//                                                editingItem = item
+//                                                viewModel.showingNewItemView = true
+//                                            }
+//                                            .tint(.blue)
 //                        }
-//                        .tint(.red)
-//                    }
-//                    .swipeActions(edge: .leading) {
-//                        Button("Edit") {
-//                            editingItem = item
-//                            viewModel.showingNewItemView = true
-//                        }
-//                        .tint(.blue)
-//                    }
 //                }
 //            }
 //            .listStyle(PlainListStyle())
@@ -134,30 +60,122 @@ struct ToDoListView_Previews: PreviewProvider {
 //            })
 //        }
 //    }
-//    
-//    //    private var filteredItems: [ToDoListItem] {
-//    //        if searchText.isEmpty {
-//    //            return items.sorted(by: { $0.dueDate > $1.dueDate })
-//    //        } else {
-//    //            return items.filter { $0.title.localizedCaseInsensitiveContains(searchText) }
-//    //                        .sorted(by: { $0.dueDate > $1.dueDate })
-//    //        }
-//    //    }
-//    //}
-//    
+//
 //    private var filteredItems: [ToDoListItem] {
 //        if searchText.isEmpty {
-//            return items.filter { !$0.isDone }.sorted(by: { $0.dueDate < $1.dueDate })
+//            return items.sorted(by: { $0.dueDate > $1.dueDate })
 //        } else {
-//            return items.filter { !$0.isDone && $0.title.localizedCaseInsensitiveContains(searchText) }
-//                .sorted(by: { $0.dueDate < $1.dueDate })
+//            return items.filter { $0.title.localizedCaseInsensitiveContains(searchText) }
+//                        .sorted(by: { $0.dueDate > $1.dueDate })
 //        }
 //    }
 //}
-//    
-//    struct ToDoListView_Previews: PreviewProvider {
-//        static var previews: some View {
-//            ToDoListView(userId: "ICm3vTb0VEX3P5rgioEWy3nhJNm1")
-//        }
-//    }
 //
+//struct ToDoListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ToDoListView(userId: "ICm3vTb0VEX3P5rgioEWy3nhJNm1")
+//    }
+//}
+//
+
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+struct ToDoListView: View {
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    @State private var searchText = ""
+    
+    @State private var editingItem: ToDoListItem? // For editing existing items
+    
+    
+    init(userId: String) {
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(filteredItems) { item in
+                    ToDoListItemView(item: item, toggleIsDone: {
+                        viewModel.toggleIsDone(item: item)
+                    })
+//                    .swipeActions(edge: .trailing) {
+//                        Button("Delete") {
+//                            viewModel.delete(id: item.id)
+//                        }
+//                        .tint(.red)
+//                    }
+                    
+                    .swipeActions(edge: .trailing) {
+                        Button(role: .destructive) {
+                            viewModel.delete(id: item.id)
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+                    }
+//                    .swipeActions(edge: .leading) {
+//                        Button("Edit") {
+//                            editingItem = item
+//                            viewModel.showingNewItemView = true
+//                        }
+//                        .tint(.blue)
+//                    }
+                    
+                    .swipeActions(edge: .leading) {
+                        Button {
+                            editingItem = item
+                            viewModel.showingNewItemView = true
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                        }
+                        .tint(.blue)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("To Do List")
+            .searchable(text: $searchText) // Using the searchable modifier
+            .toolbar {
+                Button {
+                    editingItem = nil // Reset for adding new item
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                        .foregroundColor(Color.myBlue)
+                }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView, content: {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView, itemToEdit: editingItem)
+            })
+        }
+    }
+    
+    //    private var filteredItems: [ToDoListItem] {
+    //        if searchText.isEmpty {
+    //            return items.sorted(by: { $0.dueDate > $1.dueDate })
+    //        } else {
+    //            return items.filter { $0.title.localizedCaseInsensitiveContains(searchText) }
+    //                        .sorted(by: { $0.dueDate > $1.dueDate })
+    //        }
+    //    }
+    //}
+    
+    private var filteredItems: [ToDoListItem] {
+        if searchText.isEmpty {
+            return items.filter { !$0.isDone }.sorted(by: { $0.dueDate < $1.dueDate })
+        } else {
+            return items.filter { !$0.isDone && $0.title.localizedCaseInsensitiveContains(searchText) }
+                .sorted(by: { $0.dueDate < $1.dueDate })
+        }
+    }
+}
+    
+    struct ToDoListView_Previews: PreviewProvider {
+        static var previews: some View {
+            ToDoListView(userId: "ICm3vTb0VEX3P5rgioEWy3nhJNm1")
+        }
+    }
+
